ef5901d3c0c2d31e5149ebfb6fd2c72d0b5d33f8
DaxWalker28 <dew93@drexel.edu>
2026-04-04 00:09:33 -0400

    Created five objects for Maya scene.

--- assignment/scene_builder.py
+++ assignment/scene_builder.py
@@ -1,38 +1,3 @@
-"""
-DIGM 131 - Assignment 1: Procedural Scene Builder
-==================================================
-
-OBJECTIVE:
-    Build a simple 3D scene in Maya using Python scripting.
-    You will practice using maya.cmds to create and position geometry,
-    and learn to use descriptive variable names.
-
-REQUIREMENTS:
-    1. Create a ground plane (a large, flat polygon plane).
-    2. Create at least 5 objects in your scene.
-    3. Use at least 2 different primitive types (e.g., cubes AND spheres,
-       or cylinders AND cones, etc.).
-    4. Position every object using descriptive variable names
-       (e.g., house_x, tree_height -- NOT x1, h).
-    5. Add comments explaining what each section of your code does.
-
-GRADING CRITERIA:
-    - [20%] Ground plane is created and scaled appropriately.
-    - [30%] At least 5 objects are created using at least 2 primitive types.
-    - [25%] All positions/sizes use descriptive variable names.
-    - [15%] Code is commented clearly and thoroughly.
-    - [10%] Scene is visually coherent (objects are placed intentionally,
-            not overlapping randomly).
-
-TIPS:
-    - Run this script from Maya's Script Editor (Python tab).
-    - Use maya.cmds.polyCube(), maya.cmds.polySphere(), maya.cmds.polyCylinder(),
-      maya.cmds.polyCone(), maya.cmds.polyPlane(), etc.
-    - Use maya.cmds.move(x, y, z, objectName) to position objects.
-    - Use maya.cmds.scale(x, y, z, objectName) to resize objects.
-    - Use maya.cmds.rename(oldName, newName) to give objects meaningful names.
-"""
-
 import maya.cmds as cmds
 
 # ---------------------------------------------------------------------------
@@ -78,7 +43,7 @@ building = cmds.polyCube(
 cmds.move(building_x, building_height / 2.0, building_z, building)
 
 # ---------------------------------------------------------------------------
-# TODO: Add Object 2
+# Added Roof shape.
 # Create a second object using a DIFFERENT primitive type than the cube above.
 # Remember to:
 #   - Use descriptive variable names for size and position.
@@ -104,7 +69,7 @@ cmds.rotate(90, 0, 0)
 cmds.move(roof_x, building_height, roof_z, roof)
 # moves roof on top of building.
 # ---------------------------------------------------------------------------
-# TODO: Add Object 3
+# Added sun shape.
 # ---------------------------------------------------------------------------
 sun_radius = 8
 sun_y_position = 8
@@ -123,7 +88,7 @@ cmds.move(0, 14, -14, sun)
 cmds.scale (0.25, 0.25, 0.25)
 # Shrinks sun down by 0.25.
 # ---------------------------------------------------------------------------
-# TODO: Add Object 4
+# Created trunk shape.
 # ---------------------------------------------------------------------------
 trunk_radius = 2
 trunk_height = 6
@@ -145,7 +110,7 @@ cmds.scale (0.2, 1, 0.2)
 cmds.move (trunk_x, building_height /3, trunk_z, trunk)
 # moves trunk in the opposite end of the building and roof, and makes sure it is aligned with the floor.
 # ---------------------------------------------------------------------------
-# TODO: Add Object 5
+# Added leaves shape
 # ---------------------------------------------------------------------------
 leaves_radius = 2
 leaves_y_position = 2
@@ -162,7 +127,7 @@ leaves = cmds.polySphere(
 cmds.move (leaves_x, trunk_height /2, leaves_z, leaves)
 # Moves leaves to be on top of the trunk.
 # ---------------------------------------------------------------------------
-# TODO (Optional): Add more objects to make your scene more interesting!
+
 # Consider: trees, lamp posts, fences, vehicles, animals, etc.
 # ---------------------------------------------------------------------------
 
