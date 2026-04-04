b54f805e4e2370bd9c1397fbad87ad6c53ee2ac8
DaxWalker28 <dew93@drexel.edu>
2026-04-03 23:58:47 -0400

    Update scene_builder.py

--- assignment/scene_builder.py
+++ assignment/scene_builder.py
@@ -85,23 +85,82 @@ cmds.move(building_x, building_height / 2.0, building_z, building)
 #   - Name the object meaningfully with the 'name' parameter or cmds.rename().
 #   - Position it so it sits on the ground (not floating or buried).
 # ---------------------------------------------------------------------------
+roof_width = 6
+roof_depth = 4
+roof_y_position = 4
+roof_x = -8
+roof_z = 5
 
 
+roof = cmds.polyCube(
+   name="roof",
+   width=roof_width,
+   height=roof_depth,
+   subdivisionsX=1,
+   subdivisionsY=1,
+)[0]
+# Creates roof shape.
+cmds.rotate(90, 0, 0)
+cmds.move(roof_x, building_height, roof_z, roof)
+# moves roof on top of building.
 # ---------------------------------------------------------------------------
 # TODO: Add Object 3
 # ---------------------------------------------------------------------------
+sun_radius = 8
+sun_y_position = 8
+sun_x = 8
+sun_z = 8
 
-
+sun = cmds.polySphere(
+   name="sun",
+   radius=sun_radius,
+   subdivisionsX=1,
+   subdivisionsY=1,
+)[0]
+# Creates sun shape.
+cmds.move(0, 14, -14, sun)
+# Moves sun up and backwards.
+cmds.scale (0.25, 0.25, 0.25)
+# Shrinks sun down by 0.25.
 # ---------------------------------------------------------------------------
 # TODO: Add Object 4
 # ---------------------------------------------------------------------------
+trunk_radius = 2
+trunk_height = 6
+trunk_depth = 4
+trunk_x = 8
+trunk_z = -5
 
 
+trunk = cmds.polyCylinder(
+   name="trunk",
+   radius=trunk_radius,
+   height=trunk_depth,
+   subdivisionsX=1,
+   subdivisionsY=1,
+)[0]
+# Creates trunk shape.
+cmds.scale (0.2, 1, 0.2)
+# Scales trunk to be thinner.
+cmds.move (trunk_x, building_height /3, trunk_z, trunk)
+# moves trunk in the opposite end of the building and roof, and makes sure it is aligned with the floor.
 # ---------------------------------------------------------------------------
 # TODO: Add Object 5
 # ---------------------------------------------------------------------------
+leaves_radius = 2
+leaves_y_position = 2
+leaves_x = 8
+leaves_z = -5
 
-
+leaves = cmds.polySphere(
+   name="leaves",
+   radius=leaves_radius,
+   subdivisionsX=1,
+   subdivisionsY=1,
+)[0]
+# Creates leaves shape.
+cmds.move (leaves_x, trunk_height /2, leaves_z, leaves)
+# Moves leaves to be on top of the trunk.
 # ---------------------------------------------------------------------------
 # TODO (Optional): Add more objects to make your scene more interesting!
 # Consider: trees, lamp posts, fences, vehicles, animals, etc.
