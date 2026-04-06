be6461e37cb84f10946a287026afc921f72fdc78
DaxWalker28 <dew93@drexel.edu>
2026-04-06 00:16:56 -0400

    Update scene_builder.py

--- assignment/scene_builder.py
+++ assignment/scene_builder.py
@@ -44,12 +44,8 @@ cmds.move(building_x, building_height / 2.0, building_z, building)
 
 # ---------------------------------------------------------------------------
 # Added Roof shape.
-# Create a second object using a DIFFERENT primitive type than the cube above.
-# Remember to:
-#   - Use descriptive variable names for size and position.
-#   - Name the object meaningfully with the 'name' parameter or cmds.rename().
-#   - Position it so it sits on the ground (not floating or buried).
 # ---------------------------------------------------------------------------
+# Creates roof shape.
 roof_width = 6
 roof_depth = 4
 roof_y_position = 4
@@ -64,13 +60,13 @@ roof = cmds.polyCube(
    subdivisionsX=1,
    subdivisionsY=1,
 )[0]
-# Creates roof shape.
+# Moves roof on top of building.
 cmds.rotate(90, 0, 0)
 cmds.move(roof_x, building_height, roof_z, roof)
-# moves roof on top of building.
 # ---------------------------------------------------------------------------
 # Added sun shape.
 # ---------------------------------------------------------------------------
+# Creates sun shape.
 sun_radius = 8
 sun_y_position = 8
 sun_x = 8
@@ -82,14 +78,14 @@ sun = cmds.polySphere(
    subdivisionsX=1,
    subdivisionsY=1,
 )[0]
-# Creates sun shape.
-cmds.move(0, 14, -14, sun)
 # Moves sun up and backwards.
-cmds.scale (0.25, 0.25, 0.25)
+cmds.move(0, 14, -14, sun)
 # Shrinks sun down by 0.25.
+cmds.scale (0.25, 0.25, 0.25)
 # ---------------------------------------------------------------------------
 # Created trunk shape.
 # ---------------------------------------------------------------------------
+# Creates trunk shape.
 trunk_radius = 2
 trunk_height = 6
 trunk_depth = 4
@@ -104,14 +100,14 @@ trunk = cmds.polyCylinder(
    subdivisionsX=1,
    subdivisionsY=1,
 )[0]
-# Creates trunk shape.
-cmds.scale (0.2, 1, 0.2)
 # Scales trunk to be thinner.
-cmds.move (trunk_x, building_height /3, trunk_z, trunk)
+cmds.scale (0.2, 1, 0.2)
 # moves trunk in the opposite end of the building and roof, and makes sure it is aligned with the floor.
+cmds.move (trunk_x, building_height /3, trunk_z, trunk)
 # ---------------------------------------------------------------------------
 # Added leaves shape
 # ---------------------------------------------------------------------------
+# Creates leaves shape.
 leaves_radius = 2
 leaves_y_position = 2
 leaves_x = 8
@@ -123,18 +119,8 @@ leaves = cmds.polySphere(
    subdivisionsX=1,
    subdivisionsY=1,
 )[0]
-# Creates leaves shape.
-cmds.move (leaves_x, trunk_height /2, leaves_z, leaves)
 # Moves leaves to be on top of the trunk.
-# ---------------------------------------------------------------------------
-
-# Consider: trees, lamp posts, fences, vehicles, animals, etc.
-# ---------------------------------------------------------------------------
-
-
-# ---------------------------------------------------------------------------
-# Frame All -- so the whole scene is visible in the viewport.
-# (This is provided for you -- do not remove.)
+cmds.move (leaves_x, trunk_height /2, leaves_z, leaves)
 # ---------------------------------------------------------------------------
 cmds.viewFit(allObjects=True)
 print("Scene built successfully!")
